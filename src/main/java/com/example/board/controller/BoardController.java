035568fcc99b7875ed6fef9f858775e0927e76cf
kk787016 <wnsdnr1327@naver.com>
2025-02-12 23:12:59 +0900

    front_ref

---
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -0,0 +1,35 @@
+package com.example.board.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+
+    // @Autowired
+    // private BoardService boardService;
+
+    @GetMapping("/boards")
+    public String list() {
+        return "board/list";
+    }
+
+    @GetMapping("boards/new")
+    public String createForm() {
+        return "board/create";
+    }
+
+    @GetMapping("boards/{id}")
+    public String detail() {
+        return "board/detail";
+    }
+    @GetMapping("/register")
+    public String registerForm() {
+        return "user/register";
+    }
+    @GetMapping("/login")
+    public String loginForm() {
+        return "user/login";
+    }
+}
